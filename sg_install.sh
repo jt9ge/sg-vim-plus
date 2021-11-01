3180d6b2ff2d96d4b5a262b355a6ebccf30b46f3
jim <jim>
2021-11-02 03:05:43 +0400

    初始化

---
+++ sg_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 初始化目录和空文件
+mkdir -p ~/.vim/autoload
+mkdir -p ~/.vim/plugged
+touch ~/.vimrc
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+if [ ! -d "~/.vim/plugged/molokai/colors" ]; then
+    git clone https://github.com/tomasr/molokai.git --depth=1 ~/.vim/plugged/molokai
+fi
+
+# 不管存在不，先删除再说.
+rm -f ~/.vimrc
+curl https://github.com/jt9ge/sg-vim-plus/blob/main/default.vimrc -o ~/.vimrc
+
+echo "你好漂亮!"
